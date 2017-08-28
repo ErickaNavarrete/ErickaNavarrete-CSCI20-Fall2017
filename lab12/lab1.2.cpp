7a2082ea3e0361ff1e41c6ddb947be705cf7efe3
Ericka Navarrete <erickainavarrete@gmail.com>
2017-08-28 19:19:27 +0000

    Lab1.2 Commit

---
+++ lab12/lab1.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>   // Preproxessor Directives
+ using namespace std;
+ 
+ int main()   //Main Function
+ {
+     cout<<" Zzzz"<<endl;
+     cout<<"     | \\    "<<endl;
+     cout<<"     / ,'.-'',-'''---,,_  "<<endl;
+     cout<<"    | , 4  ) )_    .,.( '''_" <<endl;
+     cout<<"   ' ---,,(_/-_)-'(_\\_) |___)";
+     
+     
+     
+ }
