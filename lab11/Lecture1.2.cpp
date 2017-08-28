df27a4fadc2f2e1aacf1eb4aa1b9b8013f9a0b80
Ericka Navarrete <erickainavarrete@gmail.com>
2017-08-28 20:46:47 +0000

    resumit lab1

---
+++ lab11/Lecture1.2.cpp
@@ -0,0 +1,19 @@
+//Created By: April Browne   //Comments
+//Created On: 8/22/2016
+ 
+ #include <iostream>   // Preproxessor Directives
+ using namespace std;
+ 
+ int main()   //Main Function
+ {
+     cout<<"Hello World! \nHello Ericka!"<<endl;
+     
+     
+ }
+ 
+ //Identify the following parts of the program:
+ //   preprocessor directives
+ //   main function
+ //   comments
+ //update it so that it is human readable
+ //add a second output line that says "Hello YourName!"
