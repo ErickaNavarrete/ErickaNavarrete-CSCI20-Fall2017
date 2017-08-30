1dcf39a6613177e6ff23fa9aeba6767f8f8537b7
Ericka Navarrete <erickainavarrete@gmail.com>
2017-08-30 19:36:12 +0000

    Lab1.3 Submission

---
+++ lab13/lab1.3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float BirthsPerSec = 8;
+    float DeathPerSec = -12;
+    float MigrantPerSec = 33;
+    float NetGainPerSec = 12;
+    
+    float BirthsPerMin = 60/BirthsPerSec;
+    float DeathsPerMin = 60/DeathPerSec;
+    float MigrantPerMin = 60/MigrantPerSec;
+    float NetGainPerMin = 60/NetGainPerSec;
+    
+    float BirthsInYear = 525600/BirthsPerMin;
+    float DeathPerYear = 525600/DeathsPerMin;
+    float MigrantPerYear = 525600/MigrantPerMin;
+    float NetGainPerYear = 525600/NetGainPerMin;
+    
+    int CurrentPop = 325770216;
+    int years;
+    
+    cout<< "What many years in the future do you want to see?"<<endl;
+    cin>>years; 
+    cout<<(CurrentPop*(years * (BirthsInYear+DeathPerYear+MigrantPerYear+NetGainPerYear)))<<endl;
+}
